b17d28406d9d7a3e08997449de805194bbc256c3
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-11-18 10:53:23 +0800

    Revert "added db schema"
    This reverts commit c2def859ec5fe3815923abdeb755f7333844754d.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.sql.*;
-
 public class Main {
-    // Test the connection
     public static void main(String[] args) {
-        try{
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/bankdb",
-                    "java",
-                    "password"
-            );
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-            Statement statement = con.createStatement();
-            ResultSet res = statement.executeQuery("Select * From customer");
-
-            while(res.next()){
-                System.out.println(res.getString("customer_first_name"));
-                System.out.println(res.getString("customer_last_name"));
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
     }
 }
