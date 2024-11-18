458778ef75b75778153f4fa93cb12860e86448a9
cruz-jullian <jullian100603@gmail.com>
2024-11-18 21:54:14 +0800

    case 1 has UserInfo.signUp

--- src/View/CustomerOptions.java
+++ src/View/CustomerOptions.java
@@ -1,32 +1,30 @@
 package View;
 
 import HelperClass.UserInput;
+import Model.UserInfo;
 
 public class CustomerOptions {
-    public static void showOptions(){
+    public static void showOptions() {
         System.out.println("1 - Sign Up\n2 - Login");
         int option = Integer.parseInt(UserInput.getScanner().nextLine());
         while (option < 1 || option > 2) {
-            System.out.println("\nInvalid option Choose again");
+            System.out.println("\nInvalid option. Choose again.");
             System.out.println("1 - Sign Up\n2 - Login");
             System.out.print("Choose Window: ");
             option = Integer.parseInt(UserInput.getScanner().nextLine());
         }
 
-        switch (option){
+        switch (option) {
             case 1:
+                UserInfo.signUp();
                 break;
             case 2:
+                login();
                 break;
         }
     }
 
-    public static login(){
-
+    public static void login() {
+        System.out.println("Login functionality is not yet implemented.");
     }
-
-    public static signUp(){
-
-    }
-
 }
