0217a12f262f95be3a8e451702c445b9d6a166ca
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-11-18 10:56:04 +0800

    Revert "Revert "added db schema""
    This reverts commit b17d2840

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    // Test the connection
+    public static void main(String[] args) {
+        try{
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/bankdb",
+                    "java",
+                    "password"
+            );
+
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("Select * From customer");
+
+            while(res.next()){
+                System.out.println(res.getString("customer_first_name"));
+                System.out.println(res.getString("customer_last_name"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
         }
     }
 }
