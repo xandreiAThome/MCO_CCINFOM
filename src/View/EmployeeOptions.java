38831993be19fc7d83e647f63c788acabe6ba6c6
retroduck385 <retroduck385@gmail.com>
2024-11-19 11:49:12 +0800

    Loan Options

--- src/View/EmployeeOptions.java
+++ src/View/EmployeeOptions.java
@@ -1,9 +1,11 @@
 package View;
 
 import HelperClass.UserInput;
+import Model.LoanOptions;
 
 public class EmployeeOptions {
     public static void showOptions(){
+        LoanOptions loanOptions = new LoanOptions("jdbc:mysql://localhost:3306/bankdb", "java", "password");
         System.out.println("1 - Add Loan Options");
         int option = Integer.parseInt(UserInput.getScanner().nextLine());
         while (option < 1 || option > 2) {
@@ -15,7 +17,20 @@ public class EmployeeOptions {
 
         switch (option){
             case 1:
-                break;
+                System.out.println("Input the necessary Information for the new loan type: ");
+                System.out.println("Loan Type: ");
+                String loanType = UserInput.getScanner().nextLine();
+                System.out.println("Interest Rate (decimal): ");
+                double interestRate = Double.parseDouble(UserInput.getScanner().nextLine());
+                System.out.println("Loan Duration (months): ");
+                int loanDur = Integer.parseInt(UserInput.getScanner().nextLine());
+                System.out.println("Max Loan Price: ");
+                double maxLoan = Double.parseDouble(UserInput.getScanner().nextLine());
+                System.out.println("Minimum Loan Price: ");
+                double minLoan = Double.parseDouble(UserInput.getScanner().nextLine());
+
+                loanOptions.addLoanOption(loanType,interestRate,loanDur,maxLoan,minLoan);
+
             case 2:
                 break;
         }
